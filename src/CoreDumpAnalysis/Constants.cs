e4f990e49f641b2d7cebf30159f1f95aad16d5bc
Dominik Steinbinder <dominik.steinbinder@dynatrace.com>
2017-06-06 11:27:42 +0200

    use summary.txt as primary source to get executable path

--- src/CoreDumpAnalysis/Constants.cs
+++ src/CoreDumpAnalysis/Constants.cs
@@ -2,8 +2,10 @@
 	public static class Constants {
 		public const string WRAPPER = "unwindwrapper.so";
 
-		public const string DEBUG_SYMBOL_URL_PATTERN = "http://127.0.0.1/{hash}/{file}";
+		public const string DEBUG_SYMBOL_URL_PATTERN = "http://dbg-ruxit.dynatrace.vmta/{hash}/{file}";
 
 		public const string DEBUG_SYMBOL_PATH = "../../../debug/";
+
+		public const string SUMMARY_TXT = "summary.txt";
 	}
 }
--- src/CoreDumpAnalysis/analysis/CoreLogAnalysis.cs
+++ src/CoreDumpAnalysis/analysis/CoreLogAnalysis.cs
@@ -7,6 +7,7 @@ using System.Text.RegularExpressions;
 namespace CoreDumpAnalysis.analysis {
 	public class CoreLogAnalysis {
 		private static Regex VERSION_REGEX = new Regex("\\(([\\w-\\.]+)\\)$");
+		private static Regex EXECUTABLE_REGEX = new Regex("executablePath: ([^\\s]+)");
 
 		private readonly SDResult analysisResult;
 		private readonly IFilesystem filesystem;
@@ -19,15 +20,25 @@ namespace CoreDumpAnalysis.analysis {
 		}
 
 		public void DebugAndSetResultFields() {
+			AnalyzeCoreDumpLog();
+			AnalyzeSummaryTxt();
+		}
+
+		public void AnalyzeCoreDumpLog() {
 			string logPath = coredump.Substring(0, coredump.Length - 4) + "log";
-			if(!filesystem.FileExists(logPath)) {
+			if (!filesystem.FileExists(logPath)) {
 				Console.WriteLine("No coredump log available (" + logPath + "). Skipping.");
 				return;
 			}
 			IEnumerable<string> lines = filesystem.ReadLines(logPath);
-			foreach(SDModule module in analysisResult.SystemContext.Modules) {
+			foreach (SDModule module in analysisResult.SystemContext.Modules) {
 				SetVersionIfAvailable(module, lines);
 			}
+		}
+
+		public void AnalyzeSummaryTxt() {
+			IEnumerable<string> lines = filesystem.ReadLines(Constants.SUMMARY_TXT);
+			SetExecutableIfAvailable(lines);
 		}
 
 		private void SetVersionIfAvailable(SDModule module, IEnumerable<string> lines) {
@@ -38,6 +49,18 @@ namespace CoreDumpAnalysis.analysis {
 						module.Version = match.Groups[1].Value;
 						return;
 					}
+				}
+			}
+		}
+
+		private void SetExecutableIfAvailable(IEnumerable<string> lines) {
+			foreach (string line in lines) {
+				Match match = EXECUTABLE_REGEX.Match(line);
+				if (match.Success) {
+					SDCDSystemContext context = analysisResult.SystemContext as SDCDSystemContext;
+					context.FileName = match.Groups[1].Value;
+					Console.WriteLine("Set filename to " + match.Groups[1].Value);
+					return;
 				}
 			}
 		}
