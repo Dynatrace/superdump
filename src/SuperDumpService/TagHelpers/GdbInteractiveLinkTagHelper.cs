c83265442487bd66af074e6199bcb0d1fa025f59
Dominik Steinbinder <dominik.steinbinder@dynatrace.com>
2017-07-24 16:39:39 +0200

    add docker compose for gotty consisting of a gotty image and a sdlinux with a gdb

---
+++ src/SuperDumpService/TagHelpers/GdbInteractiveLinkTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperDumpService.TagHelpers {
+	public class GdbInteractiveLinkTagHelper : TagHelper {
+		public string DumpId { get; set; }
+		public string BundleId { get; set; }
+		public string Executable { get; set; }
+		public string Command { get; set; }
+
+		public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output) {
+			output.TagName = "a";
+			// TODO replace 127.0.0.1 with settings.InteractiveGdbHost
+			string url = $"http://127.0.0.1:3000/?arg={BundleId}&arg={DumpId}&arg={Executable}";
+			if(!string.IsNullOrEmpty(Command)) {
+				url += $"&arg={Command}";
+			}
+			output.Attributes.Add("href", url);
+			return base.ProcessAsync(context, output);
+		}
+	}
+}
