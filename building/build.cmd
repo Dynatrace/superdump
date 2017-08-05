b51296b37a0dbf958c20a59741a48d8f10410752
mantis <mantis@users.noreply.github.com>
2017-08-05 01:30:30 +0100

    Support building with and without linux (i.e. docker) as part of build arguments
    "building/build.cmd" will perform a windows only build (i'm assuming people
    running this system are likely to be windows orientated
    
    "building/build.cmd linux" specifies target to include the docker components

--- building/build.cmd
+++ building/build.cmd
@@ -3,7 +3,10 @@ cd /d %~dp0
 set msb="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
 @IF NOT EXIST %msb% @ECHO COULDN'T FIND MSBUILD: %msb% (Is VS2017 installed?)
 
-%msb% msbuild.targets /l:FileLogger,Microsoft.Build.Engine;logfile=msbuild.log
+set target="Windows"
+if not "%1" == "" ( set target="%1" )
+
+%msb% msbuild.targets /l:FileLogger,Microsoft.Build.Engine;logfile=msbuild.log /target:%target%
 
 REM ugly hack for a bug in VS2017 (SuperDumpService.xml documentation not included in publishing)
 REM missing SuperDumpService.xml breaks swashbuckle
