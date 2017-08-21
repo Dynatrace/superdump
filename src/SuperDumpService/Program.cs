f1ec26c3642d2a6881d61ea29f019b9263b799b2
Dominik Steinbinder <dominik.steinbinder@dynatrace.com>
2017-08-21 16:58:03 +0200

    set MaxRequestBodySize to 2 GB to be able to upload large dumps (>30MB)

--- src/SuperDumpService/Program.cs
+++ src/SuperDumpService/Program.cs
@@ -5,7 +5,7 @@ namespace SuperDumpService {
 	public static class Program {
 		public static void Main(string[] args) {
 			var host = new WebHostBuilder()
-				.UseKestrel()
+				.UseKestrel(opt => opt.Limits.MaxRequestBodySize = 2147483648)  // 2gb
 				.UseContentRoot(Directory.GetCurrentDirectory())
 				.UseIISIntegration()
 				.UseStartup<Startup>()
