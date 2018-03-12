e17a35df6c1f66966758ef499c6eb71ff7ace3e1
Christoph Neumüller <c.neumueller@gmail.com>
2018-03-12 23:02:57 +0100

    add tagging for "abort", "purecall" and "stackoverflow"

--- src/SuperDump/Analyzers/WindowsTagAnalyzer.cs
+++ src/SuperDump/Analyzers/WindowsTagAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using SuperDump.Analyzer.Common;
 using SuperDump.Models;
+using System;
 using System.Linq;
 
 namespace SuperDump.Analyzers {
@@ -11,7 +12,25 @@ namespace SuperDump.Analyzers {
 				result.ThreadInformation.Values.Single(t => t.EngineId == result.LastEvent.ThreadId).Tags.Add(SDTag.NativeExceptionTag);
 			} else if (result.LastEvent?.Description?.StartsWith("Break instruction exception") ?? false) {
 				result.ThreadInformation.Values.Single(t => t.EngineId == result.LastEvent.ThreadId).Tags.Add(SDTag.BreakInstructionTag);
+			} else if (result.LastEvent?.Description?.StartsWith("Stack overflow") ?? false) {
+				result.ThreadInformation.Values.Single(t => t.EngineId == result.LastEvent.ThreadId).Tags.Add(SDTag.StackOverflowTag);
 			}
+		}
+
+		public override void AnalyzeFrame(SDThread thread, SDCombinedStackFrame frame) {
+			AddFrameAndThreadTagIf(thread, frame, () => frame.MethodName == "_purecall", SDTag.PureCallTag);
+			AddFrameAndThreadTagIf(thread, frame, () => frame.MethodName == "abort", SDTag.AbortTag);
+		}
+
+		/// <summary>
+		/// if <paramref name="func"/> returns true, set <paramref name="tag"/> on frame and thread
+		/// </summary>
+		private bool AddFrameAndThreadTagIf(SDThread thread, SDCombinedStackFrame frame, Func<bool> func, SDTag tag) {
+			if (!func()) return false;
+
+			frame.Tags.Add(tag);
+			thread.Tags.Add(tag);
+			return true;
 		}
 	}
 }
