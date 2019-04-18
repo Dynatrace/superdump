e65ac4bfdcd3d7de1404f51335ef1b983771ad0d
Daniel Schneider <daniel.schneider@dynatrace.com>
2019-04-18 15:09:28 +0200

    change static methods in search service to instance methods

--- src/SuperDumpService/Services/SearchService.cs
+++ src/SuperDumpService/Services/SearchService.cs
@@ -24,14 +24,14 @@ namespace SuperDumpService.Services {
 		}
 
 		public async Task<IOrderedEnumerable<DumpViewModel>> SearchBySimpleFilter(string searchFilter, bool includeSimilarities = true) {
-			var dumps = await Task.WhenAll(dumpRepo.GetAll().Select(x => ToDumpViewModel(x, dumpRepo, bundleRepo, includeSimilarities ? similarityService : null)));
+			var dumps = await Task.WhenAll(dumpRepo.GetAll().Select(x => ToDumpViewModel(x, includeSimilarities)));
 			var filtered = SimpleFilter(searchFilter, dumps).OrderByDescending(x => x.DumpInfo.Created);
 			return filtered;
 		}
 
 		public async Task<IOrderedEnumerable<DumpViewModel>> SearchByElasticFilter(string elasticSearchFilter, bool includeSimilarities = true) {
 			var searchResults = elasticService.SearchDumpsByJson(elasticSearchFilter).ToList();
-			IEnumerable<DumpViewModel> dumpViewModels = await Task.WhenAll(searchResults.Select(x => ToDumpViewModel(x, dumpRepo, bundleRepo, includeSimilarities ? similarityService : null)));
+			IEnumerable<DumpViewModel> dumpViewModels = await Task.WhenAll(searchResults.Select(x => ToDumpViewModel(x, includeSimilarities)));
 			dumpViewModels = dumpViewModels.Where(x => x != null); // if elasticsearch contains entries that arent found in repo, just filter those null entries
 			var dumpViewModelsOrdered = dumpViewModels.OrderByDescending(x => x.DumpInfo.Created);
 			return dumpViewModelsOrdered;
@@ -39,22 +39,22 @@ namespace SuperDumpService.Services {
 
 		public async Task<IOrderedEnumerable<DumpViewModel>> SearchDuplicates(DumpIdentifier id, bool includeSimilarities = true) {
 			var similarDumps = new Similarities(await similarityService.GetSimilarities(id)).AboveThresholdSimilarities().Select(x => x.Key);
-			var dumpViewModels = await Task.WhenAll(similarDumps.Select(x => ToDumpViewModel(x, dumpRepo, bundleRepo, includeSimilarities ? similarityService : null)));
+			var dumpViewModels = await Task.WhenAll(similarDumps.Select(x => ToDumpViewModel(x, includeSimilarities)));
 			var dumpViewModelsOrdered = dumpViewModels.OrderByDescending(x => x.DumpInfo.Created);
 			return dumpViewModelsOrdered;
 		}
 
-		private static async Task<DumpViewModel> ToDumpViewModel(ElasticSDResult elasticSDResult, DumpRepository dumpRepo, BundleRepository bundleRepo, SimilarityService similarityService = null) {
-			return await ToDumpViewModel(elasticSDResult.DumpIdentifier, dumpRepo, bundleRepo, similarityService);
+		private async Task<DumpViewModel> ToDumpViewModel(ElasticSDResult elasticSDResult, bool includeSimilarities = false) {
+			return await ToDumpViewModel(elasticSDResult.DumpIdentifier, includeSimilarities);
 		}
 
-		private static async Task<DumpViewModel> ToDumpViewModel(DumpIdentifier id, DumpRepository dumpRepo, BundleRepository bundleRepo, SimilarityService similarityService = null) {
-			return await ToDumpViewModel(dumpRepo.Get(id), dumpRepo, bundleRepo, similarityService);
+		private async Task<DumpViewModel> ToDumpViewModel(DumpIdentifier id, bool includeSimilarities = false) {
+			return await ToDumpViewModel(dumpRepo.Get(id), includeSimilarities);
 		}
 
-		private static async Task<DumpViewModel> ToDumpViewModel(DumpMetainfo dumpMetainfo, DumpRepository dumpRepo, BundleRepository bundleRepo, SimilarityService similarityService = null) {
+		private async Task<DumpViewModel> ToDumpViewModel(DumpMetainfo dumpMetainfo, bool includeSimilarities = false) {
 			if (dumpMetainfo == null) return null;
-			var similarities = similarityService == null ? null : new Similarities(await similarityService.GetSimilarities(dumpMetainfo.Id));
+			var similarities = !includeSimilarities ? null : new Similarities(await similarityService.GetSimilarities(dumpMetainfo.Id));
 			return new DumpViewModel(dumpMetainfo, new BundleViewModel(bundleRepo.Get(dumpMetainfo.BundleId)), similarities);
 		}
 
