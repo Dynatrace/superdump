bd02368d68c17cd976105ec6f307223569f10119
Daniel Schneider <daniel.schneider@dynatrace.com>
2019-06-27 15:48:16 +0200

    remove unnecessary usings

--- src/SuperDumpService/Models/JiraSearchQuery.cs
+++ src/SuperDumpService/Models/JiraSearchQuery.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-
-namespace SuperDumpService.Models {
+﻿namespace SuperDumpService.Models {
 	public class JiraSearchQuery {
 		public string Jql { get; set; }
 		public string[] Fields { get; set; }
