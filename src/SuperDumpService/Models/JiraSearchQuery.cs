6934fde8eab45f670f309509e44f3194ca106de6
Daniel Schneider <daniel.schneider@dynatrace.com>
2019-06-27 15:45:54 +0200

    add handling for Jira query maxResults

---
+++ src/SuperDumpService/Models/JiraSearchQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace SuperDumpService.Models {
+	public class JiraSearchQuery {
+		public string Jql { get; set; }
+		public string[] Fields { get; set; }
+		public int StartAt { get; set; }
+	}
+}
