3e02aa4ea869be287a67c2bed8acf08bf10669b6
Daniel Schneider <daniel.schneider@dynatrace.com>
2019-07-31 15:15:50 +0200

    fix svn source links

--- src/SuperDump.Common/DynatraceSourceLink.cs
+++ src/SuperDump.Common/DynatraceSourceLink.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SuperDump.Common {
 	public class DynatraceSourceLink {
+		private static readonly Regex SprintPathRegex = new Regex(@"^.*\\oa-s(\d*)\\[^\\]*\\(.*)$");
+
 		public static string GetRepoPathIfAvailable(string sourcePath) {
+			Match match;
 			if (sourcePath.Contains("sprint_")) {
 				int sprintOffset = sourcePath.IndexOf("sprint_");
 				return "branches/" + sourcePath.Substring(sprintOffset);
+			} else if ((match = SprintPathRegex.Match(sourcePath)).Success) {
+				return $"branches/sprint_{match.Groups[1]}/{match.Groups[2]}";
 			} else if (sourcePath.Contains("trunk")) {
 				int trunkOffset = sourcePath.IndexOf("trunk");
 				return sourcePath.Substring(trunkOffset);
