506e53a5e19474649db755b1d7d636ba948ca7d6
Daniel Schneider <daniel.schneider@dynatrace.com>
2019-08-05 15:40:24 +0200

    add string mapping for x86 and Amd64

--- src/SuperDump.Analyzer.Linux/Analysis/ElfHeaderAnalyzer.cs
+++ src/SuperDump.Analyzer.Linux/Analysis/ElfHeaderAnalyzer.cs
@@ -10,7 +10,7 @@ using Thinktecture.IO;
 
 namespace SuperDump.Analyzer.Linux.Analysis {
 	class ElfHeaderAnalyzer {
-		private static readonly Regex ArchitectureRegex = new Regex("^\\s*Machine:\\s*(.*)$", RegexOptions.Multiline);
+		private static readonly Regex ArchitectureRegex = new Regex("^\\s*Machine:\\s*(.+)$", RegexOptions.Multiline);
 
 		private readonly IFileInfo coredump;
 		private readonly Models.SDResult analysisResult;
@@ -20,6 +20,10 @@ namespace SuperDump.Analyzer.Linux.Analysis {
 			this.analysisResult = analysisResult ?? throw new ArgumentNullException("Analysis result must not be null!");
 		}
 
+		/// <summary>
+		/// Sets the system architecture to the "Machine" field of the ELF header, as returned by readelf -h.
+		/// </summary>
+		/// <returns></returns>
 		public async Task Analyze() {
 			using (ProcessRunner proc = await ProcessRunner.Run("readelf", new DirectoryInfo(Directory.GetCurrentDirectory()), "-h " + coredump.FullName)) {
 				if (proc.ExitCode != 0) {
@@ -28,9 +32,21 @@ namespace SuperDump.Analyzer.Linux.Analysis {
 
 				Match match = ArchitectureRegex.Match(proc.StdOut);
 				if (match.Success) {
-					 analysisResult.SystemContext.SystemArchitecture = match.Groups[1].Value;
+					analysisResult.SystemContext.SystemArchitecture = MapArchitectureDescription(match.Groups[1].Value);
 				}
 			}
+		}
+
+		private string MapArchitectureDescription(string architecture) {
+			if (architecture == "Intel 80386") {
+				return "x86";
+			}
+
+			if (architecture == "Advanced Micro Devices X86-64") {
+				return "Amd64";
+			}
+
+			return architecture;
 		}
 	}
 }
